9ed2242a57342f44b4363bd60bc8d0384e1af4e1
Sana Gul <sanashaaah4@gmail.com>
2025-02-23 16:43:36 +0500

    solution02: use of input that returns InputSignal

--- create-a-component-driven-by-inputs-1/src/app/movie-item/movie-item.component.ts
+++ create-a-component-driven-by-inputs-1/src/app/movie-item/movie-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, input } from "@angular/core";
 import { Movie } from "../model/movie.model";
 
 @Component({
@@ -6,11 +6,11 @@ import { Movie } from "../model/movie.model";
   template: `
     <div class="movie-item">
       <div>
-        <h4>{{ movie.title }}</h4>
+        <h4>{{ movie().title }}</h4>
         <small class="subtitle">
-          <span>Release date: {{ movie.release_date }} </span>
-          <span>Budget: $ {{ movie.budget }} million</span>
-          <span>Duration: {{ movie.duration }} min</span>
+          <span>Release date: {{ movie().release_date }} </span>
+          <span>Budget: $ {{ movie().budget }} million</span>
+          <span>Duration: {{ movie().duration }} min</span>
         </small>
       </div>
       <button>Details</button>
@@ -20,5 +20,6 @@ import { Movie } from "../model/movie.model";
   styleUrls: ["movie-item.component.scss"],
 })
 export class MovieItemComponent {
-  @Input() movie: Movie;
+  // @Input() movie: Movie;
+  movie = input.required<Movie>();
 }
